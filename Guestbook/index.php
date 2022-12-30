c98b732784dc0f37756e6f51ca96143b6b326ae7
Bohdan <bogmira777@gmail.com>
2022-12-30 11:39:48 +0100

    Add bootstrap, update db, add styles for page

--- Guestbook/index.php
+++ Guestbook/index.php
@@ -1,25 +1,25 @@
 <?php
 include_once "action.php";
-
 include "header.php";
+
 $out = out(5);
 print_r($out);
+echo "<hr>";
+echo "<section class=\"messages\"><div class=\"messages__list\">";
 if (count($out) > 0) {
     foreach ($out as $row) {
-
-        ?>
-<div style="margin:10px; padding:5px;width:450px;background:f0f0f0;">
-    <div style="color: #999999; border-bottom:1px solid #999999;padding:5px;">Опубликовал: <span
-            style="color: #444;font-weight: bold;"><?php echo $row['username']; ?></span></div>
-    <div style="background:#fafafa;padding:5px;"><?php echo $row['message']; ?></div>
-    <div style="color: #999999; border-top:1px solid #999999;padding:5px;">Дата публикации: <?php echo $row['date']; ?>
-    </div>
-</div>
+?>
+        <div class="messages__item">
+            <div class="message__publisher">Опубликовал: <span class="user__name"><?php echo $row['username']; ?></span></div>
+            <div class="message__content"><?php echo $row['message']; ?></div>
+            <div class="message__date">Дата публикации: <?php echo $row['date']; ?></div>
+        </div>
 <?php
-}
-} else // если ни одной записи не встретилось
+    }
+} 
+else
 {
     echo "В гостевой книге пока нет записей...<br>";
 }
-
+echo "</section>";
 include "footer.php";
